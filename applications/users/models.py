201db803ca2d378bde60fd25973186930d38ddf5
Juan David <juandaherreparra@gmail.com>
2023-03-26 15:25:11 -0500

    CustomUser model was added

--- applications/users/models.py
+++ applications/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    """
+    Custom class to modified Django's default user
+    """
+    birthdate = models.DateField(verbose_name='Fecha de nacimiento')
--- fundrivo/settings/base.py
+++ fundrivo/settings/base.py
@@ -92,7 +92,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
@@ -102,3 +102,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.CustomUser'
